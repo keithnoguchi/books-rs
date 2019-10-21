9ea7726d713d38f9de7773367e1e67ec97c1c287
Keith Noguchi <keith.noguchi@gmail.com>
2019-10-20 19:06:26 -0700

    array: slice test

--- src/array.rs
+++ src/array.rs
@@ -26,6 +26,53 @@ mod tests {
         }
     }
     #[test]
+    fn slice_on_array() {
+        struct Test {
+            name: &'static str,
+            data: [i32; 5],
+            range: (usize, usize),
+            want: Vec<i32>,
+        };
+        let tests = [
+            Test {
+                name: &"first entry only",
+                data: [1, 2, 3, 4, 5],
+                range: (0, 1),
+                want: vec![1],
+            },
+            Test {
+                name: &"first and second entries",
+                data: [1, 2, 3, 4, 5],
+                range: (0, 2),
+                want: vec![1, 2],
+            },
+            Test {
+                name: &"last entry",
+                data: [1, 2, 3, 4, 5],
+                range: (4, 5),
+                want: vec![5],
+            },
+            Test {
+                name: &"last two entries",
+                data: [1, 2, 3, 4, 5],
+                range: (3, 5),
+                want: vec![4, 5],
+            },
+            Test {
+                name: &"all entries",
+                data: [1, 2, 3, 4, 5],
+                range: (0, 5),
+                want: vec![1, 2, 3, 4, 5],
+            },
+        ];
+        for t in &tests {
+            let got = &t.data[t.range.0..t.range.1];
+            for (i, want) in t.want.iter().enumerate() {
+                debug_assert_eq!(&got[i], want, "{}", t.name);
+            }
+        }
+    }
+    #[test]
     fn largest_on_array() {
         struct Test {
             a: [i32; 5],
