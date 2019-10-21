29a1d09de9947219a932545e024248913e04d514
Keith Noguchi <keith.noguchi@gmail.com>
2019-10-20 17:15:23 -0700

    vector: len() method

--- src/vector.rs
+++ src/vector.rs
@@ -2,6 +2,52 @@
 #[cfg(test)]
 mod tests {
     #[test]
+    fn iterate_char_vector_with_next() {
+        let a = vec!['a', 'b', 'c', 'd', 'e'];
+        let mut i = a.iter();
+        assert_eq!(i.next(), Some(&'a'));
+        assert_eq!(i.next(), Some(&'b'));
+        assert_eq!(i.next(), Some(&'c'));
+        assert_eq!(i.next(), Some(&'d'));
+        assert_eq!(i.next(), Some(&'e'));
+        for _ in 1..1000 {
+            assert_eq!(i.next(), None);
+        }
+    }
+    #[test]
+    fn vector_size_with_len() {
+        struct Test {
+            name: &'static str,
+            data: Vec<i32>,
+            want: usize,
+        };
+        let tests = [
+            Test {
+                name: &"empty vector size",
+                data: vec![],
+                want: 0,
+            },
+            Test {
+                name: &"one entry vector size",
+                data: vec![1],
+                want: 1,
+            },
+            Test {
+                name: &"two entries vector size",
+                data: vec![1, 2],
+                want: 2,
+            },
+            Test {
+                name: &"three entries vector size",
+                data: vec![1, 2, 3],
+                want: 3,
+            },
+        ];
+        for t in &tests {
+            debug_assert_eq!(t.data.len(), t.want, "{}", t.want);
+        }
+    }
+    #[test]
     fn push_on_char_vector() {
         struct Test {
             name: &'static str,
@@ -34,19 +80,6 @@ mod tests {
                 t.data.push(*a);
             }
             debug_assert_eq!(t.data, t.want, "{}", t.name);
-        }
-    }
-    #[test]
-    fn iterate_char_vector_with_next() {
-        let a = vec!['a', 'b', 'c', 'd', 'e'];
-        let mut i = a.iter();
-        assert_eq!(i.next(), Some(&'a'));
-        assert_eq!(i.next(), Some(&'b'));
-        assert_eq!(i.next(), Some(&'c'));
-        assert_eq!(i.next(), Some(&'d'));
-        assert_eq!(i.next(), Some(&'e'));
-        for _ in 1..1000 {
-            assert_eq!(i.next(), None);
         }
     }
 }
