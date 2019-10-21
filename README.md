42a5be8b8876ceb61953d64a8a48a2d12873e7d5
Keith Noguchi <keith.noguchi@gmail.com>
2019-10-20 17:45:45 -0700

    README.md: Add unit test output

--- README.md
+++ README.md
@@ -6,4 +6,30 @@ Rust sandbox for fun!
 
 [CircleCI]: https://circleci.com/gh/keithnoguchi/rustbox.svg?style=svg
 
+# Test
+
+```sh
+$ make test
+    Finished dev [unoptimized + debuginfo] target(s) in 0.00s
+     Running target/debug/deps/rustbox-9e9575d91e3bd718
+
+running 6 tests
+test array::tests::largest_on_array ... ok
+test vector::tests::length_of_vector ... ok
+test array::tests::next_on_array ... ok
+test vector::tests::next_on_vector ... ok
+test vector::tests::push_on_vector ... ok
+test vector::tests::pop_from_vector ... ok
+
+test result: ok. 6 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out
+
+   Doc-tests rustbox
+
+running 0 tests
+
+test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out
+
+$
+```
+
 Happy Hacking!
--- src/array.rs
+++ src/array.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
-pub fn largest_of_five(a: &[i32; 5]) -> &i32 {
+pub fn largest(a: &[i32; 5]) -> &i32 {
     let mut largest = &a[0];
     for i in a.iter() {
         if i > largest {
@@ -12,22 +12,7 @@ pub fn largest_of_five(a: &[i32; 5]) -> &i32 {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn largest_of_five() {
-        struct Test {
-            a: [i32; 5],
-            want: i32,
-        }
-        let tests = [
-            Test {a: [1, 2, 3, 4, 5], want: 5},
-            Test {a: [5, 4, 3, 2, 1], want: 5},
-            Test {a: [1, 4, 3, 2, 5], want: 5},
-        ];
-        for t in &tests {
-            assert_eq!(super::largest_of_five(&t.a), &t.want);
-        }
-    }
-    #[test]
-    fn iterate_integer_array_with_next() {
+    fn next_on_array() {
         let a = [1, 2, 3, 4, 5];
         let mut i = a.iter();
         assert_eq!(i.next(), Some(&1));
@@ -38,6 +23,21 @@ mod tests {
         // you can call next() forever.
         for _ in 0..1000 {
             assert_eq!(i.next(), None);
+        }
+    }
+    #[test]
+    fn largest_on_array() {
+        struct Test {
+            a: [i32; 5],
+            want: i32,
+        }
+        let tests = [
+            Test {a: [1, 2, 3, 4, 5], want: 5},
+            Test {a: [5, 4, 3, 2, 1], want: 5},
+            Test {a: [1, 4, 3, 2, 5], want: 5},
+        ];
+        for t in &tests {
+            assert_eq!(super::largest(&t.a), &t.want);
         }
     }
 }
