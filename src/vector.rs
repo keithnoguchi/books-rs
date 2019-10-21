68ab5283dff777f6539e1c9b13ba304dea3938d8
Keith Noguchi <keith.noguchi@gmail.com>
2019-10-20 17:05:40 -0700

    vector: Vec.push() unit test

--- src/vector.rs
+++ src/vector.rs
@@ -2,6 +2,41 @@
 #[cfg(test)]
 mod tests {
     #[test]
+    fn push_on_char_vector() {
+        struct Test {
+            name: &'static str,
+            data: Vec<char>,
+            push: Vec<char>,
+            want: Vec<char>,
+        }
+        let mut tests = [
+            Test {
+                name: &"push to the empty vector",
+                data: vec![],
+                push: vec!['a', 'b', 'c'],
+                want: vec!['a', 'b', 'c'],
+            },
+            Test {
+                name: &"push to the existing vector",
+                data: vec!['a', 'b', 'c'],
+                push: vec!['d', 'e', 'f'],
+                want: vec!['a', 'b', 'c', 'd', 'e', 'f'],
+            },
+            Test {
+                name: &"push the duplicate data",
+                data: vec!['a', 'b', 'c'],
+                push: vec!['a', 'b', 'c'],
+                want: vec!['a', 'b', 'c', 'a', 'b', 'c'],
+            },
+        ];
+        for t in &mut tests {
+            for a in t.push.iter() {
+                t.data.push(*a);
+            }
+            debug_assert_eq!(t.data, t.want, "{}", t.name);
+        }
+    }
+    #[test]
     fn iterate_char_vector_with_next() {
         let a = vec!['a', 'b', 'c', 'd', 'e'];
         let mut i = a.iter();
