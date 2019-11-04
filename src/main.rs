2b5b5f720683a3c45d247021de1e0f906c3cb8c8
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-04 12:18:58 -0800

    main: Playing with async/.await with the async book
    https://rust-lang.github.io/async-book/print.html#applied-simple-http-server

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
 // SPDX-License-Identifier: GPL-2.0
-use futures::executor::block_on;
+//
+// https://rust-lang.github.io/async-book/print.html#applied-simple-http-server
+use {
+    hyper::{
+        Body, Client, Request, Response, Server, Uri,
+        service::service_fn,
+        rt::run,
+    },
+    futures::{
+        compat::Future01CompatExt,
+        future::{FutureExt, TryFutureExt},
+    },
+    std::net::SocketAddr,
+};
 
-async fn hello_world() {
-    println!("Hello, world!");
+async fn serve_req(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    Ok(Response::new(Body::from("hello, world")))
+}
+
+async fn run_server(addr: SocketAddr) {
+    println!("Listening on http://{}", addr);
+    let serve_future = Server::bind(&addr)
+        .serve(|| service_fn(|req| serve_req(req).boxed().compat()));
+
+    if let Err(e) = serve_future.compat().await {
+        eprintln!("server error: {}", e);
+    }
 }
 
 fn main() {
-    let future = hello_world();
-    block_on(future);
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let futures_03_future = run_server(addr);
+    let futures_01_future = futures_03_future.unit_error().boxed().compat();
+    run(futures_01_future);
 }
