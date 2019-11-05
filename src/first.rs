e17e464f364708cf20338aaa7b06e476dd15f3ce
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-04 16:09:49 -0800

    first: Add List.push()

--- src/first.rs
+++ src/first.rs
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
+use std::mem;
+
 pub struct List {
     head: Link,
 }
@@ -11,4 +13,17 @@ enum Link {
 struct Node {
     elem: i32,
     next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(new_node);
+    }
 }
