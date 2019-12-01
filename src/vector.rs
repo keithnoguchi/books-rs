cc7a90910b23e2765eaee86e2cafff60c146a456
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-30 17:20:46 -0800

    vector: Keep the original largest() function as a comparison

--- src/vector.rs
+++ src/vector.rs
@@ -46,6 +46,19 @@ impl cmp::PartialEq for Error {
 
 #[allow(dead_code)]
 fn largest<T: PartialOrd + Clone>(list: &[T]) -> Result<T, Error> {
+    if let Some(mut largest) = list.get(0) {
+        for value in list {
+            if value > largest {
+                largest = value
+            }
+        }
+        return Ok(largest.clone());
+    }
+    Err(Error::from(io::ErrorKind::InvalidInput))
+}
+
+#[allow(dead_code)]
+fn largest_iter<T: PartialOrd + Clone>(list: &[T]) -> Result<T, Error> {
     let mut i = list.iter();
     if let Some(mut largest) = i.next() {
         loop {
@@ -543,6 +556,332 @@ mod tests {
         }];
         for t in &tests {
             match largest::<String>(&t.data) {
+                Ok(_) => {
+                    let msg = format!("{}({}): unexpected success", NAME, t.name);
+                    panic!(msg)
+                }
+                Err(err) => {
+                    debug_assert_eq!(t.want, err, "{}({})", NAME, t.name);
+                }
+            }
+        }
+    }
+    #[test]
+    fn largest_iter_ok_i32() {
+        const NAME: &str = "largest_iter_ok_i32";
+        struct Test {
+            name: &'static str,
+            data: Vec<i32>,
+            want: i32,
+        }
+        let tests = [
+            Test {
+                name: "single element vector",
+                data: vec![1],
+                want: 1,
+            },
+            Test {
+                name: "ascending two elements vector",
+                data: vec![1, 2],
+                want: 2,
+            },
+            Test {
+                name: "descending two elements vector",
+                data: vec![2, 1],
+                want: 2,
+            },
+            Test {
+                name: "ascending five elements vector",
+                data: vec![1, 2, 3, 4, 5],
+                want: 5,
+            },
+            Test {
+                name: "decending five elements vector",
+                data: vec![5, 4, 3, 2, 1],
+                want: 5,
+            },
+            Test {
+                name: "unsorted five elements vector",
+                data: vec![1, 5, 3, 2, 4],
+                want: 5,
+            },
+        ];
+        for t in &tests {
+            match largest_iter::<i32>(&t.data) {
+                Err(err) => {
+                    let msg = format!("{}({}): {}", NAME, t.name, err);
+                    panic!("{}", msg);
+                }
+                Ok(got) => {
+                    debug_assert_eq!(t.want, got, "{}({})", NAME, t.name);
+                }
+            }
+        }
+    }
+    #[test]
+    fn largest_iter_err_i32() {
+        const NAME: &str = "largest_iter_err_i32";
+        struct Test {
+            name: &'static str,
+            data: Vec<i32>,
+            want: Error,
+        }
+        let tests = [Test {
+            name: "empty i32 vector",
+            data: vec![],
+            want: Error::from(std::io::ErrorKind::InvalidInput),
+        }];
+        for t in &tests {
+            match largest_iter::<i32>(&t.data) {
+                Ok(_) => {
+                    let msg = format!("{}({}): unexpected success", NAME, t.name);
+                    panic!(msg)
+                }
+                Err(err) => {
+                    debug_assert_eq!(t.want, err, "{}({})", NAME, t.name);
+                }
+            }
+        }
+    }
+    #[test]
+    fn largest_iter_ok_f32() {
+        const NAME: &str = "largest_iter_ok_f32";
+        struct Test {
+            name: &'static str,
+            data: Vec<f32>,
+            want: f32,
+        }
+        let tests = [
+            Test {
+                name: "single element vector",
+                data: vec![1.1],
+                want: 1.1,
+            },
+            Test {
+                name: "ascending two elements vector",
+                data: vec![1.1, 2.1],
+                want: 2.1,
+            },
+            Test {
+                name: "descending two elements vector",
+                data: vec![2.9, 1.1],
+                want: 2.9,
+            },
+            Test {
+                name: "ascending five elements vector",
+                data: vec![1.1, 2.1, 3.1, 4.1, 5.1],
+                want: 5.1,
+            },
+            Test {
+                name: "decending five elements vector",
+                data: vec![5.9, 4.9, 3.9, 2.9, 1.9],
+                want: 5.9,
+            },
+            Test {
+                name: "unsorted five elements vector",
+                data: vec![1.5, 5.5, 3.5, 2.5, 4.5],
+                want: 5.5,
+            },
+        ];
+        for t in &tests {
+            match largest_iter::<f32>(&t.data) {
+                Err(err) => {
+                    let msg = format!("{}({}): {}", NAME, t.name, err);
+                    panic!("{}", msg);
+                }
+                Ok(got) => {
+                    debug_assert_eq!(t.want, got, "{}({})", NAME, t.name);
+                }
+            }
+        }
+    }
+    #[test]
+    fn largest_iter_err_f32() {
+        const NAME: &str = "largest_iter_err_f32";
+        struct Test {
+            name: &'static str,
+            data: Vec<f32>,
+            want: Error,
+        }
+        let tests = [Test {
+            name: "empty f32 vector",
+            data: vec![],
+            want: Error::from(std::io::ErrorKind::InvalidInput),
+        }];
+        for t in &tests {
+            match largest_iter::<f32>(&t.data) {
+                Ok(_) => {
+                    let msg = format!("{}({}): unexpected success", NAME, t.name);
+                    panic!(msg)
+                }
+                Err(err) => {
+                    debug_assert_eq!(t.want, err, "{}({})", NAME, t.name);
+                }
+            }
+        }
+    }
+    #[test]
+    fn largest_iter_ok_char() {
+        const NAME: &str = "largest_iter_ok_char";
+        struct Test {
+            name: &'static str,
+            data: Vec<char>,
+            want: char,
+        }
+        let tests = [
+            Test {
+                name: "single element vector",
+                data: vec!['a'],
+                want: 'a',
+            },
+            Test {
+                name: "ascending two elements vector",
+                data: vec!['a', 'b'],
+                want: 'b',
+            },
+            Test {
+                name: "descending two elements vector",
+                data: vec!['b', 'a'],
+                want: 'b',
+            },
+            Test {
+                name: "ascending five elements vector",
+                data: vec!['a', 'b', 'c', 'd', 'e'],
+                want: 'e',
+            },
+            Test {
+                name: "decending five elements vector",
+                data: vec!['e', 'd', 'c', 'b', 'a'],
+                want: 'e',
+            },
+            Test {
+                name: "unsorted five elements vector",
+                data: vec!['a', 'e', 'b', 'c', 'd'],
+                want: 'e',
+            },
+        ];
+        for t in &tests {
+            match largest_iter::<char>(&t.data) {
+                Err(err) => {
+                    let msg = format!("{}({}): {}", NAME, t.name, err);
+                    panic!("{}", msg);
+                }
+                Ok(got) => {
+                    debug_assert_eq!(t.want, got, "{}({})", NAME, t.name);
+                }
+            }
+        }
+    }
+    #[test]
+    fn largest_iter_err_char() {
+        const NAME: &str = "largest_iter_err_char";
+        struct Test {
+            name: &'static str,
+            data: Vec<char>,
+            want: Error,
+        }
+        let tests = [Test {
+            name: "empty char vector",
+            data: vec![],
+            want: Error::from(std::io::ErrorKind::InvalidInput),
+        }];
+        for t in &tests {
+            match largest_iter::<char>(&t.data) {
+                Ok(_) => {
+                    let msg = format!("{}({}): unexpected success", NAME, t.name);
+                    panic!(msg)
+                }
+                Err(err) => {
+                    debug_assert_eq!(t.want, err, "{}({})", NAME, t.name);
+                }
+            }
+        }
+    }
+    #[test]
+    fn largest_iter_ok_string() {
+        const NAME: &str = "largest_iter_ok_string";
+        struct Test {
+            name: &'static str,
+            data: Vec<String>,
+            want: String,
+        }
+        let tests = [
+            Test {
+                name: "single element vector",
+                data: vec!["a".to_string()],
+                want: "a".to_string(),
+            },
+            Test {
+                name: "ascending two elements vector",
+                data: vec!["a".to_string(), "b".to_string()],
+                want: "b".to_string(),
+            },
+            Test {
+                name: "descending two elements vector",
+                data: vec!["b".to_string(), "a".to_string()],
+                want: "b".to_string(),
+            },
+            Test {
+                name: "ascending five elements vector",
+                data: vec![
+                    "a".to_string(),
+                    "b".to_string(),
+                    "c".to_string(),
+                    "d".to_string(),
+                    "e".to_string(),
+                ],
+                want: "e".to_string(),
+            },
+            Test {
+                name: "decending five elements vector",
+                data: vec![
+                    "e".to_string(),
+                    "d".to_string(),
+                    "c".to_string(),
+                    "b".to_string(),
+                    "a".to_string(),
+                ],
+                want: "e".to_string(),
+            },
+            Test {
+                name: "unsorted five elements vector",
+                data: vec![
+                    "a".to_string(),
+                    "e".to_string(),
+                    "b".to_string(),
+                    "c".to_string(),
+                    "d".to_string(),
+                ],
+                want: "e".to_string(),
+            },
+        ];
+        for t in &tests {
+            match largest_iter::<String>(&t.data) {
+                Err(err) => {
+                    let msg = format!("{}({}): {}", NAME, t.name, err);
+                    panic!("{}", msg);
+                }
+                Ok(got) => {
+                    debug_assert_eq!(t.want, got, "{}({})", NAME, t.name);
+                }
+            }
+        }
+    }
+    #[test]
+    fn largest_iter_err_string() {
+        const NAME: &str = "largest_iter_err_string";
+        struct Test {
+            name: &'static str,
+            data: Vec<String>,
+            want: Error,
+        }
+        let tests = [Test {
+            name: "empty string vector",
+            data: vec![],
+            want: Error::from(std::io::ErrorKind::InvalidInput),
+        }];
+        for t in &tests {
+            match largest_iter::<String>(&t.data) {
                 Ok(_) => {
                     let msg = format!("{}({}): unexpected success", NAME, t.name);
                     panic!(msg)
