b54da587f7571231730a9bc4c597fa1de22bd0a2
Keith Noguchi <keith.noguchi@gmail.com>
2019-12-01 16:27:53 -0800

    test: Add documentation with assert_eq!() based example

--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0
+/// Adds two to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = rustbox::add_two(arg);
+/// assert_eq!(7, answer);
+/// ```
 pub fn add_two(x: i32) -> i32 {
     x + 2
 }
