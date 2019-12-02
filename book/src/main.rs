76dcb41dd37448eeaafd27ce5355d9eeb03a54f3
Keith Noguchi <keith.noguchi@gmail.com>
2019-12-01 23:55:28 -0800

    book/main: Handle error message in main()

--- book/src/main.rs
+++ book/src/main.rs
@@ -1,12 +1,30 @@
 // SPDX-License-Identifier: GPL-2.0
 // https://doc.rust-lang.org/stable/book/ch12-00-an-io-project.html
 use book::{ch12::Config, Error};
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
-    let cfg = Config::new(&args)?;
+    let cfg = Config::new(&args).unwrap_or_else(|err| usage(err));
     let contents = fs::read_to_string(cfg.filename())?;
     println!("With text:\n{}", contents);
     Ok(())
+}
+
+fn usage(err: Error) -> ! {
+    const PROGNAME: &str = "book";
+    use std::io;
+    match err {
+        Error::Io(err) => match err.kind() {
+            io::ErrorKind::InvalidInput => {
+                eprintln!("{}: {}", PROGNAME, err.to_string());
+            }
+            io::ErrorKind::NotFound => {
+                eprintln!("{}: {}", PROGNAME, err.to_string());
+            }
+            _ => eprintln!("{}: oops {:?}", PROGNAME, err),
+        },
+        _ => eprintln!("{}: unexpected error", PROGNAME),
+    }
+    process::exit(1);
 }
