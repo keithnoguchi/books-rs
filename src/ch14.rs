f07b30acfdf64ae86000187c51c86268c50d093e
Keith Noguchi <keith.noguchi@gmail.com>
2019-12-01 18:16:29 -0800

    ch14: Adding pub use behavior

--- src/ch14.rs
+++ src/ch14.rs
@@ -3,8 +3,11 @@
 
 pub mod art {
     //! A library for modeling artistic concepts
+    pub use self::kinds::PrimaryColor;
+    pub use self::kinds::SecondaryColor;
     pub mod kinds {
         /// The primary colors according to the RYB color model.
+        #[derive(PartialEq, Debug)]
         pub enum PrimaryColor {
             Red,
             Yellow,
@@ -17,13 +20,40 @@ pub mod art {
             Purple,
         }
     }
-
     pub mod utils {
         use super::kinds::*;
         /// Combines two primary colors in equal amounts to create
         /// a secondary color.
         pub fn mix(_c1: PrimaryColor, _c2: PrimaryColor) -> SecondaryColor {
             SecondaryColor::Orange
+        }
+    }
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        #[test]
+        fn check_primary_color_re_exports() {
+            struct Test {
+                color: PrimaryColor,
+                want: PrimaryColor,
+            }
+            let tests = [
+                Test {
+                    color: kinds::PrimaryColor::Red,
+                    want: PrimaryColor::Red,
+                },
+                Test {
+                    color: kinds::PrimaryColor::Yellow,
+                    want: PrimaryColor::Yellow,
+                },
+                Test {
+                    color: kinds::PrimaryColor::Blue,
+                    want: PrimaryColor::Blue,
+                },
+            ];
+            for t in &tests {
+                assert_eq!(t.want, t.color);
+            }
         }
     }
 }
