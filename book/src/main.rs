2cc780c3810cbcc3d66aca646edf684b7f9191de
Keith Noguchi <keith.noguchi@gmail.com>
2019-12-01 20:13:53 -0800

    book/main: Start to implement the I/O project
    https://doc.rust-lang.org/stable/book/ch12-00-an-io-project.html

--- book/src/main.rs
+++ book/src/main.rs
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
+// https://doc.rust-lang.org/stable/book/ch12-00-an-io-project.html
+use std::env;
+
 fn main() {
-    println!("Hello world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 }
