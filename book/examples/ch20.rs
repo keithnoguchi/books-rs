6aa72c0ccbc3e4273aedfe3b3ee268857fa03085
Keith Noguchi <keith.noguchi@gmail.com>
2019-12-24 13:14:12 -0800

    book/ch20: Start to implement WorkQueue

--- book/examples/ch20.rs
+++ book/examples/ch20.rs
@@ -1,15 +1,10 @@
 //! Final Project: Building a Multithreaded Web Server
-use std::net::TcpListener;
-use the_book::ch20;
+use the_book::ch20::WorkQueue;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let _pool = ch20::ThreadPool::new(2);
-    let l = TcpListener::bind("localhost:7878")?;
-    for s in l.incoming() {
-        match s {
-            Err(e) => return Err(Box::new(e)),
-            Ok(_s) => println!("got the connection!"),
-        }
-    }
+    let wq = WorkQueue::new(1);
+    wq.exec(|| {
+        println!("Hello WorkQueue!");
+    });
     Ok(())
 }
--- book/src/ch20.rs
+++ book/src/ch20.rs
@@ -1,14 +1,49 @@
 //! Chapter 20 Final Project: Building a Multithreaded Web Server
-pub struct ThreadPool {
-    _workers: Vec<Worker>,
+
+/// fixed sized worker queue.
+pub struct WorkQueue {
+    size: usize,
+    workers: Vec<Worker>,
+}
+
+impl WorkQueue {
+    /// new creates size number of workers.
+    ///
+    /// # Panics
+    ///
+    /// It will panic if the size is zero.
+    pub fn new(size: usize) -> Self {
+        assert!(size != 0);
+        let mut workers = Vec::with_capacity(size);
+        for _ in 0..size {
+            workers.push(Worker {});
+        }
+        Self { size, workers }
+    }
+    /// execute executes the closure on one of the workers.
+    pub fn exec<F>(&self, f: F) -> Result<(), Box<dyn std::error::Error>>
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        f();
+        Ok(())
+    }
 }
 
 struct Worker;
 
-impl ThreadPool {
-    pub fn new(size: usize) -> Self {
-        Self {
-            _workers: Vec::with_capacity(size),
-        }
+#[cfg(test)]
+mod tests {
+    use super::WorkQueue;
+    #[test]
+    fn new() {
+        let wq = WorkQueue::new(1);
+        assert_eq!(1, wq.size);
+    }
+    fn exec() {
+        let wq = WorkQueue::new(1);
+        wq.exec(|| {
+            println!("hello");
+        });
     }
 }
