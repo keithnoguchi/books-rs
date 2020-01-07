80e872200256b4fe0fc84a4ea098dfdee618af5f
Keith Noguchi <keith.noguchi@gmail.com>
2020-01-06 17:44:21 -0800

    embedded/examples/rc.rs: std::rc::Rc example

---
+++ embedded/examples/rc.rs
@@ -0,0 +1,44 @@
+use std::fmt;
+use std::rc::Rc;
+
+struct Foo(String);
+
+impl Foo {
+    fn new(name: &str) -> Self {
+        Self(name.to_string())
+    }
+}
+
+impl fmt::Display for Foo {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Drop for Foo {
+    fn drop(&mut self) {
+        println!("dropping {}", self.0);
+    }
+}
+
+fn main() {
+    let x = Rc::new(Foo::new("foo"));
+    bar(x.clone());
+    baz(&*x);
+    let x = Rc::new(75);
+    assert_eq!(75, *x);
+}
+
+fn bar<T>(x: T)
+where
+    T: fmt::Display
+{
+    println!("bar: {}", x);
+}
+
+fn baz<T>(x: T)
+where
+    T: fmt::Display
+{
+    println!("baz: {}", x);
+}
