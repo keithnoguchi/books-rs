e4ac890319f97c90fab116735bc70ce665e49334
Keith Noguchi <keith.noguchi@gmail.com>
2020-01-12 12:05:48 -0800

    book/ch17/blog: Add the ch17-blog.rs example

---
+++ book/examples/ch17-blog.rs
@@ -0,0 +1,14 @@
+//! Implemented an [Object-Oriented Design Pattern]
+//!
+//! [object-oriented design pattern]: https://doc.rust-lang.org/book/ch17-03-oo-design-patterns.html
+use std::error::Error;
+
+use the_book::ch17::blog::Post;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut post = Post::new();
+    post.add_text("Let's start the blog");
+    post.add_text("oops, let's add more text to the first blog entry");
+    println!("Here is the published content: '{}'", post.content());
+    Ok(())
+}
