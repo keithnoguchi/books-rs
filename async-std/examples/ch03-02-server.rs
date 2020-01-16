4061be965fcd6ad909018472cfcbdb658b62298b
Keith Noguchi <keith.noguchi@gmail.com>
2020-01-16 13:38:00 -0800

    async-std/examples/ch03-02-server: Listen and accept the connection

--- async-std/examples/ch03-02-server.rs
+++ async-std/examples/ch03-02-server.rs
@@ -1,15 +1,27 @@
-//! [Chat] server
+//! Writing an [accept loop]
 //!
-//! [chat]: https://book.async.rs/tutorial/index.html
-use async_std::{net::ToSocketAddrs, task};
+//! # Examples
+//!
+//! ```rust
+//! $ cargo run --example ch03-02-server
+//! Compiling async-std-book v0.1.0 (/home/kei/git/books-rs/async-std)
+//! Finished dev [unoptimized + debuginfo] target(s) in 0.77s
+//! Running `/home/kei/git/books-rs/target/debug/examples/ch03-02-server`
+//! listening on TcpListener { watcher: Watcher { entry: Entry { token: Token(1), readers: Mutex { data: [] }, writers: Mutex { data: [] } }, source: Some(TcpListener { addr: V6([::1]:3000), fd: 3 }) } }
+//! accepted the connection with Ok(TcpStream { watcher: Watcher { entry: Entry { token: Token(2), readers: Mutex { data: [] }, writers: Mutex { data: [] } }, source: Some(TcpStream { addr: V6([::1]:3000), peer: V6([::1]:57726), fd: 7 }) } })
+//! accepted the connection with Ok(TcpStream { watcher: Watcher { entry: Entry { token: Token(2), readers: Mutex { data: [] }, writers: Mutex { data: [] } }, source: Some(TcpStream { addr: V6([::1]:3000), peer: V6([::1]:57728), fd: 7 }) } })
+//! ...
+//! ```
+//!
+//! [accept loop]: https://book.async.rs/tutorial/accept_loop.html
+use async_std::net::{TcpListener, ToSocketAddrs};
+use async_std::stream::StreamExt;
+use async_std::task;
 use std::{env, error, result};
 
 type Result<T> = result::Result<T, Box<dyn error::Error + Send + Sync>>;
 
 fn main() -> Result<()> {
-    // Simple [argument parsing].
-    //
-    // [argument parsing]: https://doc.rust-lang.org/rust-by-example/std_misc/arg/matching.html
     let argv: Vec<String> = env::args().collect();
     let addr = match argv.len() {
         2 => &argv[1],
@@ -18,8 +30,14 @@ fn main() -> Result<()> {
     task::block_on(server(addr))
 }
 
+/// `server` listens on the `addr` SocketAddr and accepts the TCP connection
+/// from the client.
 async fn server(addr: impl ToSocketAddrs) -> Result<()> {
-    let addr = addr.to_socket_addrs().await?.next();
-    println!("{:?}", addr);
+    let server = TcpListener::bind(addr).await?;
+    eprintln!("listening on {:?}", server);
+    let mut incoming = server.incoming();
+    while let Some(s) = incoming.next().await {
+        eprintln!("accepted the connection with {:?}", s);
+    }
     Ok(())
 }
