7c0cb4507d826ac45216f8c40bb92f4339cf55f2
Keith Noguchi <keith.noguchi@gmail.com>
2020-02-11 00:04:27 -0800

    wasm/ch03: Add the static global variable

--- wasm/src/ch03/mod.rs
+++ wasm/src/ch03/mod.rs
@@ -1,3 +1,13 @@
 //! Creating Rusty Checkers
 pub mod board;
 pub mod game;
+
+use game::Engine;
+
+use lazy_static::lazy_static;
+use mut_static::MutStatic;
+
+lazy_static! {
+    pub static ref GAME_ENGINE: MutStatic<Engine> =
+    { MutStatic::from(Engine::new()) };
+}
