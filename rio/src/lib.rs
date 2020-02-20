4c0d1891fda954ae843c16c49624a718c95dde53
Keith Noguchi <keith.noguchi@gmail.com>
2020-02-19 22:04:33 -0800

    rio: io_uring for Rust

---
+++ rio/src/lib.rs
@@ -0,0 +1,5 @@
+//! [rio]: misuse-resistant bindings for [io_uring]
+//!
+//! [rio]: https://lib.rs/crates/rio
+//! [io_uring]: https://kernel.dk/io_uring.pdf
+//! [liburing]: https://github.com/axboe/liburing
