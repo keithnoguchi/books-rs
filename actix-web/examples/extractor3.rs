ec3a118c1de265379c484f927698009637462ffa
Keith Noguchi <keith.noguchi@gmail.com>
2020-03-08 17:38:54 -0700

    actix-web/examples/extractor3: Query based path extractor example

---
+++ actix-web/examples/extractor3.rs
@@ -0,0 +1,20 @@
+//! Query based path [extractor] example
+//!
+//! [extractor]: https://actix.rs/docs/extractors/
+use std::io;
+
+use actix_web::{web, App, Result, HttpRequest, HttpServer};
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| App::new().route("/users/{id}/{name}", web::get().to(users)))
+        .bind("127.0.0.1:8088")?
+        .run()
+        .await
+}
+
+async fn users(req: HttpRequest) -> Result<String> {
+    let name: String = req.match_info().get("name").unwrap().parse().unwrap();
+    let id: u32 = req.match_info().get("id").unwrap().parse().unwrap();
+    Ok(format!("Welcome {}, userid {}", name, id))
+}
