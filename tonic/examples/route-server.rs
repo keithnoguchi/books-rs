936480f54919c222439a56da3f6193d450dbc5c4
Keith Noguchi <keith.noguchi@gmail.com>
2020-03-12 22:48:14 -0700

    tonic/examples/route-server: Implement the RouteGuide trait

---
+++ tonic/examples/route-server.rs
@@ -0,0 +1,50 @@
+//! [Route] guide server
+//!
+//! [route]: https://github.com/hyperium/tonic/blob/master/examples/routeguide-tutorial.md
+use std::{error, pin::Pin};
+
+use futures_core::stream::Stream;
+use tokio::sync::mpsc;
+use tonic::{Request, Response, Status};
+
+use tonic_book::{
+    route::{Feature, Point, Rectangle, RouteNote, RouteSummary},
+    route_guide_server::RouteGuide,
+};
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let svc = RouteGuideService {};
+    println!("RouteGuideService = {:?}!", svc);
+    Ok(())
+}
+
+#[derive(Debug)]
+struct RouteGuideService;
+
+#[tonic::async_trait]
+impl RouteGuide for RouteGuideService {
+    async fn get_feature(&self, _req: Request<Point>) -> Result<Response<Feature>, Status> {
+        todo!();
+    }
+    type ListFeaturesStream = mpsc::Receiver<Result<Feature, Status>>;
+    async fn list_features(
+        &self,
+        _req: Request<Rectangle>,
+    ) -> Result<Response<Self::ListFeaturesStream>, Status> {
+        todo!();
+    }
+    async fn record_route(
+        &self,
+        _req: Request<tonic::Streaming<Point>>,
+    ) -> Result<Response<RouteSummary>, Status> {
+        todo!();
+    }
+    type RouteChatStream =
+        Pin<Box<dyn Stream<Item = Result<RouteNote, Status>> + Send + Sync + 'static>>;
+    async fn route_chat(
+        &self,
+        _req: Request<tonic::Streaming<RouteNote>>,
+    ) -> Result<Response<Self::RouteChatStream>, Status> {
+        todo!();
+    }
+}
--- tonic/src/lib.rs
+++ tonic/src/lib.rs
@@ -3,3 +3,4 @@
 //! [tonic]: https://lib.rs/tonic
 pub mod hello;
 pub mod route;
+pub use route::route_guide_server;
