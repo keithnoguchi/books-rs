ee87dc3947c908d8c2312adda62e18735262e50a
Keith Noguchi <keith.noguchi@gmail.com>
2020-07-26 23:01:49 -0700

    book/example/ch02: Number guessing game

--- book/examples/ch02.rs
+++ book/examples/ch02.rs
@@ -1,11 +1,29 @@
-// SPDX-License-Identifier: GPL-2.0
-use std::{error, io};
+use rand::Rng;
+use std::{cmp::Ordering, error, io};
+
+const MIN: u32 = 1;
+const MAX: u32 = 100;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
-    println!("Guess the number!");
-    println!("Please input your guess");
+    println!("Guess the number between {} and {}", MIN, MAX);
+    let secret = rand::thread_rng().gen_range(MIN, MAX + 1);
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess)?;
-    println!("You guessed: {}", guess);
+    while let Ok(n) = io::stdin().read_line(&mut guess) {
+        if n == 0 {
+            break;
+        }
+        match guess.trim().parse::<u32>() {
+            Err(_) => println!("type number, try again"),
+            Ok(n) => match n.cmp(&secret) {
+                Ordering::Less => println!("Higher"),
+                Ordering::Greater => println!("Lower"),
+                Ordering::Equal => {
+                    println!("Bingo");
+                    break;
+                }
+            },
+        }
+        guess.clear();
+    }
     Ok(())
 }
