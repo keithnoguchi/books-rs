9cf806c91aa3ef6fb696f7a1b9911cc8cf1a5834
Keith Noguchi <keith.noguchi@gmail.com>
2020-07-29 16:43:00 -0700

    book/examples/ch03: Ch03: Common Programming Concepts

--- book/examples/ch03.rs
+++ book/examples/ch03.rs
@@ -1,24 +1,13 @@
 //! [Common Programming Concept]
 //!
 //! [common programming concept]: https://doc.rust-lang.org/book/ch03-00-common-programming-concepts.html
-
 fn main() {
-    count_down(3);
     let a = [10, 20, 30, 40, 50];
-    count_up(&a);
-}
-
-fn count_down(number: usize) {
-    println!("rev() based loop");
-    for i in (0..number).rev() {
-        println!("{}", i + 1);
-    }
-    println!("LIFTOFF!!");
-}
-
-fn count_up(a: &[i32]) {
-    println!("iter() based loop");
     for i in a.iter() {
         println!("{}", i);
     }
+    for i in (1..4).rev() {
+        println!("{}", i);
+    }
+    println!("LIFTOFF!");
 }
