58437c551c86633e64cb8d1de2fe3cb3bb56b79a
Keith Noguchi <keith.noguchi@gmail.com>
2020-08-13 14:42:32 -0700

    book/ch10/sec01: Generic type parameter example in struct methods

--- book/examples/ch10-point.rs
+++ book/examples/ch10-point.rs
@@ -4,13 +4,18 @@
 use the_book::ch10::Point;
 
 fn main() {
-    let p = Point { x: 1, y: 2 };
-    assert_eq!(1, p.x());
-    assert_eq!(2, p.y());
-    let p = Point { x: 1, y: 2.0 };
-    assert_eq!(1, p.x());
-    assert_eq!(2.0, p.y());
-    let p = Point { x: "one", y: "two" };
-    assert_eq!("one", p.x());
-    assert_eq!("two", p.y());
+    let p = Point::new(1, 2);
+    assert_eq!(&1, p.x());
+    assert_eq!(&2, p.y());
+    let p = Point::new(1, 2.0);
+    assert_eq!(&1, p.x());
+    assert_eq!(&2.0, p.y());
+    let p = Point::new("one", "two");
+    assert_eq!(&"one", p.x());
+    assert_eq!(&"two", p.y());
+    let p = Point::new(String::from("hello"), 1.1);
+    let q = Point::new(1, String::from("world"));
+    let r = p.mixup(q);
+    assert_eq!("hello", r.x());
+    assert_eq!("world", r.y());
 }
--- book/src/ch10/sec01.rs
+++ book/src/ch10/sec01.rs
@@ -1,45 +1,17 @@
 //! [Generic Data Types]
 //!
 //! [generic data types]: https://doc.rust-lang.org/book/ch10-01-syntax.html
-//!
-//! # Examples
-//!
-//! ```
-//! use the_book::ch10::sec01::largest;
-//!
-//! let list = ['a', 'b', 'c', 'd', 'e'];
-//! assert_eq!(&'e', largest(&list).unwrap());
-//! let list: [i32; 0] = [];
-//! assert_eq!(None, largest(&list));
-//! ```
-//!
-//! `Point` generic strucutre with the same types.
-//!
-//! ```
-//! use the_book::ch10::Point;
-//!
-//! assert_eq!(1, Point { x: 1, y: 2 }.x);
-//! assert_eq!(2.0, Point { x: 1.0, y: 2.0}.y);
-//! assert_eq!('a', Point { x: 'a', y: 'b'}.x);
-//! ```
-//!
-//! `Point` generic structure with the different types.
-//!
-//! ```
-//! use the_book::ch10::Point;
-//!
-//! assert_eq!(1, Point { x: 1, y: 'a' }.x);
-//! assert_eq!('a', Point { x: 1.0, y: 'a' }.y);
-//! ```
-//!
-//! Generic methods.
-//!
-//! ```
-//! use the_book::ch10::Point;
-//!
-//! assert_eq!(1, Point { x: 1, y: 'a' }.x());
-//! assert_eq!(2.0, Point { x: 1.1, y: 2.0 }.y());
-//! ```
+
+/// # Examples
+///
+/// ```
+/// use the_book::ch10::sec01::largest;
+///
+/// let list = ['a', 'b', 'c', 'd', 'e'];
+/// assert_eq!(&'e', largest(&list).unwrap());
+/// let list: [i32; 0] = [];
+/// assert_eq!(None, largest(&list));
+/// ```
 pub fn largest<T: PartialOrd>(list: &[T]) -> Option<&T> {
     let mut largest = list.first()?;
     for x in list.iter() {
@@ -50,25 +22,58 @@ pub fn largest<T: PartialOrd>(list: &[T]) -> Option<&T> {
     Some(largest)
 }
 
-/// Double type point structure.
-pub struct Point<T, U = T>
-where
-    T: Copy,
-    U: Copy,
-{
-    pub x: T,
-    pub y: U,
+/// # Examples
+///
+/// `Point` generic strucutre with the same types.
+///
+/// ```
+/// use the_book::ch10::Point;
+///
+/// assert_eq!(&1, Point::new(1, 2).x());
+/// assert_eq!(&2.0, Point::new(1.0, 2.0).y());
+/// assert_eq!(&'a', Point::new('a', 'b').x())
+/// ```
+///
+/// `Point` generic structure with the different types.
+///
+/// ```
+/// use the_book::ch10::Point;
+///
+/// assert_eq!(&1, Point::new(1, 'a').x());
+/// assert_eq!(&'b', Point::new(1.0, 'b').y());
+/// ```
+///
+/// `mixup` `Point` method to showcase the method generic parameter
+///
+/// ```
+/// use the_book::ch10::Point;
+///
+/// let p = Point::new(1, 5.4);
+/// let q = Point::new('a', String::from("hello"));
+///
+/// let r = p.mixup(q);
+/// assert_eq!(&1, r.x());
+/// assert_eq!("hello", r.y());
+/// ```
+pub struct Point<T, U> {
+    x: T,
+    y: U,
 }
 
-impl<T, U> Point<T, U>
-where
-    T: Copy,
-    U: Copy,
-{
-    pub fn x(&self) -> T {
-        self.x
+impl<T, U> Point<T, U> {
+    pub fn new(x: T, y: U) -> Self {
+        Self { x, y }
     }
-    pub fn y(&self) -> U {
-        self.y
+    pub fn x(&self) -> &T {
+        &self.x
+    }
+    pub fn y(&self) -> &U {
+        &self.y
+    }
+    pub fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
 }
