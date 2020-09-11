86f225e1a6c595a4c1de657bd65877c8f858460e
Keith Noguchi <keith.noguchi@gmail.com>
2020-09-11 13:46:10 -0700

    books/ch13: Iterator adapter example with .map().collect()

--- book/examples/ch13-02-map.rs
+++ book/examples/ch13-02-map.rs
@@ -2,8 +2,7 @@
 //!
 //! [iterators]: https://doc.rust-lang.org/book/ch13-02-iterators.html
 fn main() {
-    // The `map()` iterator adaptor example which adapts i32 value to f32.
-    let v = vec![4i32, 6, 8];
-    let v2: Vec<f32> = v.iter().map(|x| (*x as f32) / 4.0).collect();
-    assert_eq!(vec![1.0f32, 1.5, 2.0], v2);
+    let v = vec![1, 2, 3];
+    let i = v.iter().map(|x| (*x as f32) / 4.0);
+    assert_eq!(vec![0.25, 0.5, 0.75], i.collect::<Vec<f32>>());
 }
